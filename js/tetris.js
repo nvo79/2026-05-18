e9b5dfd7175e69a8690e3a340bdfc01a681308df
Keijiro Tsurumaru <251205119@ccmailg.meijo-u.ac.jp>
2026-05-18 14:18:10 +0900

    色を変更

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'skyblue', 'lavender', 'aqua', 'chocolate', 'pink', 'lime', 'indigo'
+    'skyblue', 'lavender', 'aqua', 'chocolate', 'gold', 'lime', 'indigo'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
